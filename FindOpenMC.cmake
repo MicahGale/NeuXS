fd095d6db042d9264e4cf31a2b8e78abcd4dd033
Ebny Walid Ahammed <69362074+magnoxemo@users.noreply.github.com>
2026-03-20 18:48:51 -0500

    Refactor OpenMC CMake configuration for better clarity
    there was no headers in include/openmc except version.h

--- FindOpenMC.cmake
+++ FindOpenMC.cmake
@@ -1,19 +1,29 @@
-set(OPENMC_INSTALL_DIR "$ENV{OPENMC_INSTALL_DIR}")
+set(OPENMC_BUILD_DIR "$ENV{OPENMC_INSTALL_DIR}")
+set(OPENMC_SOURCE_DIR "$ENV{OPENMC_INSTALL_DIR}")
 
 find_package(HDF5 REQUIRED)
 
-if(EXISTS "${OPENMC_INSTALL_DIR}")
-    set(OPENMC_FOUND TRUE)
+if(EXISTS "${OPENMC_BUILD_DIR}")
+    set(OPENMC_LIB_FOUND TRUE)
+    set(OPENMC_LIBRARIES "${OPENMC_BUILD_DIR}/lib/libopenmc.so")
 
-    set(OPENMC_INCLUDE_DIRS "${OPENMC_INSTALL_DIR}/include/openmc/")
-    set(OPENMC_LIBRARIES "${OPENMC_INSTALL_DIR}/lib/libopenmc.so")
-
-    message(STATUS "OpenMC includes: ${OPENMC_INCLUDE_DIRS}")
     message(STATUS "OpenMC library: ${OPENMC_LIBRARIES}")
 
 else()
-    set(OPENMC_FOUND FALSE)
-    message( "OpenMC not found at ${OPENMC_INSTALL_DIR}")
+    set(OPENMC_LIB_FOUND FALSE)
+    message( "OpenMC lib not found at ${OPENMC_INSTALL_DIR}")
+endif()
+
+
+if(EXISTS "${OPENMC_SOURCE_DIR}")
+    set(OPENMC_SRC_FOUND TRUE)
+    set(OPENMC_INCLUDE_DIRS "${OPENMC_SOURCE_DIR}/include/")
+
+    message(STATUS "OpenMC library: ${OPENMC_LIBRARIES}")
+
+else()
+    set(OPENMC_SRC_FOUND FALSE)
+    message( "OpenMC source directory not found at ${OPENMC_SOURCE_DIR}")
 endif()
 
 
