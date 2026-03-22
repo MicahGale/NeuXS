2ddfbd755df641121bfdc3a6b5dbec4470e9b1c5
magnoxemo <walid.ahammed01@gmail.com>
2026-03-21 21:38:12 -0500

    fix return type

--- tests/test_load_openmc.cpp
+++ tests/test_load_openmc.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     openmc::Particle neuxs_demo_particle;
-    auto epic_particle_id = neuxs_demo_particle.id();
+    int64_t epic_particle_id = neuxs_demo_particle.id();
 
     printf("neuxs epic particle id %d\n",static_cast<int>(epic_particle_id));
     // by default id should be -1
