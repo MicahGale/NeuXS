912d37a325e5956c79673b49179a986d68f7303b
Micah Gale <mgale@montepy.org.>
2026-04-09 22:23:19 -0500

    Hashed out framework for compile-time dispatched material.

--- include/material.cuh
+++ include/material.cuh
@@ -1,4 +1,25 @@
 #ifndef NEUXS_MATERIAL_H
 #define NEUXS_MATERIAL_H
 
-#endif //NEUXS_MATERIAL_H
+#endif // NEUXS_MATERIAL_H
+
+namespace neuxs {
+template <typename Derived> class Material {
+public:
+  __device__ void get_total_cross_section(float *energies, float *sigma_t,
+                                          unsigned int n_particles);
+  __device__ void decide_if_collide(float *energies, float *distance_escape,
+                                    bool *collide, unsigned int n_particles);
+  __device__ void
+  decide_collide_type(float *energies,
+                      neuxs::CrossSectionDataType *collision_types,
+                      unsigned int n_particles);
+};
+
+class ArrayStructMat : public Material<ArrayStructMat> {};
+
+class StructArrayMat : public Material<StructArrayMat> {};
+
+class SLBWMat : public Material<SLBWMat> {};
+
+} // namespace neuxs
--- include/transport.cuh
+++ include/transport.cuh
@@ -1,4 +1,9 @@
 #ifndef NEUXS_TRANSPORT_CUH
 #define NEUXS_TRANSPORT_CUH
 
-#endif //NEUXS_TRANSPORT_CUH
+#endif // NEUXS_TRANSPORT_CUH
+
+namespace neuxs {
+public
+enum class EventType { COLLIDE, ESCAPE, DIE };
+} // namespace neuxs
