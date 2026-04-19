f97c93a901424fba706925ab07dfb1fa4d4f92cd
magnoxemo <walid.ahammed01@gmail.com>
2026-04-19 11:35:50 -0500

    using better template naming

--- include/cross_section.cuh
+++ include/cross_section.cuh
@@ -17,8 +17,10 @@ namespace neuxs {
 
 class OpenMCCrossSectionReader;
 
-template <typename T> using DeviceVector = thrust::device_vector<T>;
-template <typename T> using HostVector = thrust::host_vector<T>;
+template <typename FPrecision>
+using DeviceVector = thrust::device_vector<FPrecision>;
+template <typename FPrecision>
+using HostVector = thrust::host_vector<FPrecision>;
 
 // ============= Main play ground for different data structure ==========
 // =======================================================================
@@ -27,16 +29,17 @@ template <typename T> using HostVector = thrust::host_vector<T>;
  * explore for AoS data structure. Energy grid needs to be separate.
  */
 
-template <typename T> struct CrossSectionGridPoint {
+template <typename FPrecision> struct CrossSectionGridPoint {
 
-  CrossSectionGridPoint(T sigma_s, T sigma_f, T sigma_c)
+  CrossSectionGridPoint(FPrecision sigma_s, FPrecision sigma_f,
+                        FPrecision sigma_c)
       : _sigma_s(sigma_s), _sigma_f(sigma_f), _sigma_c(sigma_c),
         _sigma_t(_sigma_c + _sigma_f + _sigma_s) {}
 
-  T _sigma_s;
-  T _sigma_f;
-  T _sigma_c;
-  T _sigma_t;
+  FPrecision _sigma_s;
+  FPrecision _sigma_f;
+  FPrecision _sigma_c;
+  FPrecision _sigma_t;
 };
 
 /*
@@ -45,10 +48,11 @@ template <typename T> struct CrossSectionGridPoint {
  * vector.
  */
 
-template <typename T> struct CrossSectionArray {
+template <typename FPrecision> struct CrossSectionArray {
   CrossSectionArray() = default;
-  CrossSectionArray(DeviceVector<T> sigma_s, DeviceVector<T> sigma_f,
-                    DeviceVector<T> sigma_c)
+  CrossSectionArray(DeviceVector<FPrecision> sigma_s,
+                    DeviceVector<FPrecision> sigma_f,
+                    DeviceVector<FPrecision> sigma_c)
       : _sigma_s(sigma_s), _sigma_f(sigma_f), _sigma_c(sigma_c) {
 
     _sigma_t.resize(_sigma_s.size());
@@ -56,26 +60,26 @@ template <typename T> struct CrossSectionArray {
       _sigma_t[i] = _sigma_s[i] + _sigma_f[i] + _sigma_c[i];
   }
 
-  DeviceVector<T> _sigma_s;
-  DeviceVector<T> _sigma_f;
-  DeviceVector<T> _sigma_c;
-  DeviceVector<T> _sigma_t;
+  DeviceVector<FPrecision> _sigma_s;
+  DeviceVector<FPrecision> _sigma_f;
+  DeviceVector<FPrecision> _sigma_c;
+  DeviceVector<FPrecision> _sigma_t;
 };
 
-template <typename T> struct HashMap {};
+template <typename FPrecision> struct HashMap {};
 
 // ===================Cross section Base class ====================
 //              This is only for a single nuclide
 // ================================================================
 
 /*
- * Base class for cross-section data type. T1 will be cross-section data
- * structure.T2 will numeric data type. either float of double. All the daughter
- * class will just declare the T1 based on which cross-section data structure
- * they are using.
+ * Base class for cross-section data type. XSType will be cross-section data
+ * structure. FPrecision will numeric data type. either float of double. All the
+ * daughter class will just declare the T1 based on which cross-section data
+ * structure they are using.
  */
 
-template <typename T1, typename T2> class CrossSection {
+template <typename XSType, typename FPrecision> class CrossSection {
 public:
   /* cross setter from OpenMCCrossSectionReader object
    * this method will use the OpenMCCrossSectionReader object
@@ -89,47 +93,54 @@ public:
   /* It needs to be abstract as we will implement
    * different kind of interpolation methods
    */
-  __device__ virtual CrossSectionGridPoint<T2> getCrossSection(T2 *energy) = 0;
+  __device__ virtual void
+  getCrossSection(FPrecision *energy,
+                  CrossSectionGridPoint<FPrecision> *xs_grid) = 0;
 
   /* play ground for different grid search methodology
    */
-  __device__ virtual size_t searchEnergyGrid(T2 *energy) = 0;
+  __device__ virtual size_t searchEnergyGrid(FPrecision *energy) = 0;
 
   // Energy grid. I need to talk to Micah about this
   // design and there could be a future change if we decide to  use energy
   // grid's lethargy value for search. Just an idea.
 
-  DeviceVector<T2> _energy;
+  DeviceVector<FPrecision> _energy;
 };
 
-template <typename T>
-class AoSLinear : public CrossSection<CrossSectionGridPoint<T>, T> {
+template <typename FPrecision>
+class AoSLinear
+    : public CrossSection<CrossSectionGridPoint<FPrecision>, FPrecision> {
 
 public:
   __host__ virtual void setCrossSection(const OpenMCCrossSectionReader &reader,
                                         NuclideComponent &nuclide) override;
 
-  __device__ virtual size_t searchEnergyGrid(T *energy) override {};
-  // linear-linear interpolation methods here
-  __device__ virtual CrossSectionGridPoint<T>
-  getCrossSection(T *energy) override {};
+  __device__ virtual size_t searchEnergyGrid(FPrecision *energy) override {};
 
-  DeviceVector<CrossSectionGridPoint<T>> _device_data;
+  // linear-linear interpolation methods here
+  __device__ virtual void
+  getCrossSection(FPrecision *energy,
+                  CrossSectionGridPoint<FPrecision> *xs_grid) override {};
+
+  DeviceVector<CrossSectionGridPoint<FPrecision>> _device_data;
 };
 
-template <typename T> class SoALinear : CrossSection<CrossSectionArray<T>, T> {
+template <typename FPrecision>
+class SoALinear : CrossSection<CrossSectionArray<FPrecision>, FPrecision> {
 
 public:
   __host__ virtual void setCrossSection(const OpenMCCrossSectionReader &reader,
                                         NuclideComponent &nuclide) override;
 
-  __device__ virtual size_t searchEnergyGrid(T *energy) override {};
+  __device__ virtual size_t searchEnergyGrid(FPrecision *energy) override {};
 
   // linear-linear interpolation methods here
-  __device__ virtual CrossSectionGridPoint<T>
-  getCrossSection(T *energy) override {};
+  __device__ virtual void
+  getCrossSection(FPrecision *energy,
+                  CrossSectionGridPoint<FPrecision> *xs_grid) override {};
 
-  CrossSectionArray<T> _device_data;
+  CrossSectionArray<FPrecision> _device_data;
 };
 
 } // namespace neuxs
--- src/cross_section.cu
+++ src/cross_section.cu
@@ -8,81 +8,85 @@
 
 namespace neuxs {
 
-template <typename T>
-void AoSLinear<T>::setCrossSection(const OpenMCCrossSectionReader &reader,
-                                   NuclideComponent &nuclide) {
+template <typename FPrecision>
+void AoSLinear<FPrecision>::setCrossSection(
+    const OpenMCCrossSectionReader &reader, NuclideComponent &nuclide) {
 
   auto nuclide_name = std::string(nuclide._name);
-  auto energy_host =
-      reader.getEnergyDataPoints<T>(nuclide_name, nuclide._temperature);
+  auto energy_host = reader.getEnergyDataPoints<FPrecision>(
+      nuclide_name, nuclide._temperature);
   const auto size = energy_host.size();
 
-  this->_energy = DeviceVector<T>(energy_host.begin(), energy_host.end());
+  this->_energy =
+      DeviceVector<FPrecision>(energy_host.begin(), energy_host.end());
 
-  auto scattering = reader.getCrossSectionDataPoints<T>(
+  auto scattering = reader.getCrossSectionDataPoints<FPrecision>(
       nuclide_name, nuclide._temperature, CrossSectionDataType::SCATTERING);
 
-  auto capture = reader.getCrossSectionDataPoints<T>(
+  auto capture = reader.getCrossSectionDataPoints<FPrecision>(
       nuclide_name, nuclide._temperature, CrossSectionDataType::CAPTURE);
 
-  std::vector<T> fission;
+  std::vector<FPrecision> fission;
   if (nuclide._allows_fission) {
-    fission = reader.getCrossSectionDataPoints<T>(
+    fission = reader.getCrossSectionDataPoints<FPrecision>(
         nuclide_name, nuclide._temperature, CrossSectionDataType::FISSION);
     if (fission.empty())
       fission.reserve(size);
 
     if (fission.empty()) {
-      fission.assign(size, static_cast<T>(0));
+      fission.assign(size, static_cast<FPrecision>(0));
     }
   } else {
-    fission.assign(size, static_cast<T>(0));
+    fission.assign(size, static_cast<FPrecision>(0));
   }
 
   for (std::size_t i = 0; i < size; i++) {
-    this->_device_data.push_back(
-        CrossSectionGridPoint<T>(scattering[i], fission[i], capture[i]));
+    this->_device_data.push_back(CrossSectionGridPoint<FPrecision>(
+        scattering[i], fission[i], capture[i]));
   }
 }
 
-template <typename T>
-void SoALinear<T>::setCrossSection(const OpenMCCrossSectionReader &reader,
-                                   NuclideComponent &nuclide) {
+template <typename FPrecision>
+void SoALinear<FPrecision>::setCrossSection(
+    const OpenMCCrossSectionReader &reader, NuclideComponent &nuclide) {
   auto nuclide_name = std::string(nuclide._name);
 
-  auto energy_host =
-      reader.getEnergyDataPoints<T>(nuclide_name, nuclide._temperature);
+  auto energy_host = reader.getEnergyDataPoints<FPrecision>(
+      nuclide_name, nuclide._temperature);
   const auto size = energy_host.size();
 
-  this->_energy = DeviceVector<T>(energy_host.begin(), energy_host.end());
+  this->_energy =
+      DeviceVector<FPrecision>(energy_host.begin(), energy_host.end());
 
-  auto scattering_host = reader.getCrossSectionDataPoints<T>(
+  auto scattering_host = reader.getCrossSectionDataPoints<FPrecision>(
       nuclide_name, nuclide._temperature, CrossSectionDataType::SCATTERING);
 
-  auto capture_host = reader.getCrossSectionDataPoints<T>(
+  auto capture_host = reader.getCrossSectionDataPoints<FPrecision>(
       nuclide_name, nuclide._temperature, CrossSectionDataType::CAPTURE);
 
-  std::vector<T> fission_host;
+  std::vector<FPrecision> fission_host;
   if (nuclide._allows_fission) {
-    fission_host = reader.getCrossSectionDataPoints<T>(
+    fission_host = reader.getCrossSectionDataPoints<FPrecision>(
         nuclide_name, nuclide._temperature, CrossSectionDataType::FISSION);
 
     if (fission_host.empty())
-      fission_host.assign(size, static_cast<T>(0));
+      fission_host.assign(size, static_cast<FPrecision>(0));
 
   } else
-    fission_host.assign(size, static_cast<T>(0));
+    fission_host.assign(size, static_cast<FPrecision>(0));
 
   // I kinda agree now that when I made the cross-section reader I made some
   // poor choice
-  DeviceVector<T> scattering_device(scattering_host.begin(),
-                                    scattering_host.end());
-  DeviceVector<T> fission_device(fission_host.begin(), fission_host.end());
-  DeviceVector<T> capture_device(capture_host.begin(), capture_host.end());
+  DeviceVector<FPrecision> scattering_device(scattering_host.begin(),
+                                             scattering_host.end());
+  DeviceVector<FPrecision> fission_device(fission_host.begin(),
+                                          fission_host.end());
+  DeviceVector<FPrecision> capture_device(capture_host.begin(),
+                                          capture_host.end());
 
   // The constructor will compute sigma_t = sigma_s + sigma_f + sigma_c
-  this->_device_data =
-      CrossSectionArray<T>(scattering_device, fission_device, capture_device);
+  this->_device_data = CrossSectionArray<FPrecision>(
+      scattering_device, fission_device, capture_device);
 }
 
 // need explicit definition otherwise compiler goes wild
