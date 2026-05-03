2d34dbd0673cf8dd669db00778bcf46f6c193f31
magnoxemo <walid.ahammed01@gmail.com>
2026-05-02 21:40:27 -0500

    adding slurm script

--- benchmark/pincell_benchmark.sh
+++ benchmark/pincell_benchmark.sh
@@ -8,11 +8,12 @@ F_PRECISION="single"
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=0
 #SBATCH --cpus-per-task=1
-#SBATCH --time=00:30:00
+#SBATCH --time=00:10:00
 #SBATCH --gres=gpu:1
-#SBATCH --output=%x.out
-#SBATCH --error=%x.err
+#SBATCH --output=%${XS_DATA}_${F_PRECISION}.out
+#SBATCH --error=%${XS_DATA}_${F_PRECISION}.err
 
-module load nvidia/cuda/13.0.0
+# if have run the setup.sh then the ENV is already ready to go
 
-export OPENMC_CROSS_SECTIONS='some path'
+cd "${PWD}/../build/benchmark"
+./pincell "$XS_DATA" "$F_PRECISION"
